4955ec7f10d79bf2c38c996aaf5d997bede0db54
Francisco <flalonso17@esei.uvigo.es>
2019-10-05 22:31:47 +0200

    Nuevos Archivos Añadidos
    Se han añadido las funciones desconectar y autenticación de usuarios. Además se ha separado el pie de página y la cabecera en dos únicos archivos para inluirlos en la vistas. De esta manera, se evita la sobrecarga de código en la misma

---
+++ Views/Header.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Pádel Web
+    </title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <script src='main.js'></script>
+</head>
+<body>
+
+    <!--Navegación----->
+
+    <section id="nav-bar">
+        <nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="#"><strong>PÁDELESEI</strong><img src="img/logo.png" width="40" height="40"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">INICIO</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#services">SERVICIOS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#testimonials">TESTIOMONIOS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">UBICACION</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+        
+    </section>

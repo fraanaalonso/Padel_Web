940031aaf26c58d117b48eb92d688d086c4ca9ee
Francisco <55636363+fraanaalonso@users.noreply.github.com>
2019-10-16 21:29:31 +0200

    Update HeaderPost.php

--- Views/HeaderPost.php
+++ Views/HeaderPost.php
@@ -35,7 +35,7 @@ $login = $_SESSION['login'];
     <a href="../Controllers/Court_Controller.php"><li><span class="lnr lnr-page-break icon2"></span><h4 class="text2">Pistas</h4></li></a>
     <li><span class="lnr lnr-book icon3"></span><h4 class="text3">Reservas</h4></li>
     <li><span class="lnr lnr-license icon4"></span><h4 class="text4">Campeonatos</h4></li>
-    <li><span class="lnr lnr-diceicon6"></span><h4 class="text6">Enfrentamiento</h4></li> 
+    <li><span class="lnr lnr-dice icon6"></span><h4 class="text6">Enfrentamiento</h4></li> 
     <li><span class="lnr lnr-heart icon7"></span><h4 class="text7">Sobre Nosotros</h4></li> 
     <li><span class="lnr lnr-question-circle icon8"></span><h4 class="text8">FAQ</h4></li> 
     <a href="../Functions/Desconect.php"><li ><span class="lnr lnr-exit icon9"></span><h4  class="text9">Salir</h4></li> </a>

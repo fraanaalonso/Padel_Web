282cc014318f75c35b4f0b9fdf0e7ff8a01d8c6f
Francisco <55636363+fraanaalonso@users.noreply.github.com>
2021-01-23 11:35:42 +0100

    Update Notification_Controller.php

--- Controllers/Notification_Controller.php
+++ Controllers/Notification_Controller.php
@@ -23,8 +23,8 @@ $mail = new PHPMailer(true);
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'eseipadel19@gmail.com';                     // SMTP username
-    $mail->Password   = 'esei19padel';                               // SMTP password
+    $mail->Username   = '';                     // SMTP username
+    $mail->Password   = '';                               // SMTP password
     $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
